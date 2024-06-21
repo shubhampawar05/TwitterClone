6050d77644be7250dea95b0c70b0418e920d5e61
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-21 23:46:38 +0530

    add

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Right from "../../components/right/Right";
 import Feed from "../../components/feed/Feed";
 import Left from "../../components/left.jsx/Left";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import useOtherUser from "../../hook/useOtherUser";
 import useGetMyTweets from "../../hook/useGetMyTweets";
+import { useSelector } from "react-redux";
 
 
 const Home = () => {
+
+
   useOtherUser();
   useGetMyTweets();
+  
+  const { user } = useSelector(store => store.user);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if (!user) {
+      navigate("/login");
+    }
+  },[]);
+  
   return (
     <>
       <div className=" flex max-w-screen-xl mx-auto ">
